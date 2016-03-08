dabe62e29ecf30810fa6ee4065fe13d979b88d64
ferdinan <312683956@qq.com>
2016-03-09 01:20:46 +0800

    update redis

--- advanced/common/config/main.php
+++ advanced/common/config/main.php
@@ -4,6 +4,18 @@ return [
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
-        ]
-    ]
+        ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => 'localhost',
+            'post' => 6379,
+            'database' => 0,
+        ],
+        'cache' => [
+            'class' => 'yii\redis\Cache',
+        ],
+        'session' => [
+            'class' => 'yii\redis\Session',
+        ],
+    ],
 ];
