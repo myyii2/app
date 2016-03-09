0f90cce9a5a9be59ccbf263d30f27812ed2471bc
ferdinan <312683956@qq.com>
2016-03-10 01:42:54 +0800

    update new project

--- advanced/common/config/main.php
+++ advanced/common/config/main.php
@@ -4,18 +4,6 @@ return [
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
-        ],
-        'redis' => [
-            'class' => 'yii\redis\Connection',
-            'hostname' => 'localhost',
-            'post' => 6379,
-            'database' => 0,
-        ],
-        'cache' => [
-            'class' => 'yii\redis\Cache',
-        ],
-        'session' => [
-            'class' => 'yii\redis\Session',
-        ],
+        ]
     ],
 ];
